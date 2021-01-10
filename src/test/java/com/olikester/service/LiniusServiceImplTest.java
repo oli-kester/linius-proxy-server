d030405ace70370f4996264ca96005036d116251
Oliver Reynolds <oli_kester@zoho.com>
2021-01-10 17:32:45 +0000

    Wrote tests for remaining REST functions, and added test data.

--- src/test/java/com/olikester/service/LiniusServiceImplTest.java
+++ src/test/java/com/olikester/service/LiniusServiceImplTest.java
@@ -2,6 +2,9 @@ package com.olikester.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -26,11 +29,17 @@ class LiniusServiceImplTest {
     private LiniusService liniusService;
 
     private LiniusAccessToken testAccessToken;
-    static final MultiValueMap<String, String> validSearchParams1 = new LinkedMultiValueMap<>();
+    private static final MultiValueMap<String, String> validSearchParams1 = new LinkedMultiValueMap<>();
+    private static final MultiValueMap<String, String> validEnrichJobsParams1 = new LinkedMultiValueMap<>();
+    private static String discoverBody;
+    private static String enrichBody;
 
     @BeforeAll
     static void setUpBeforeClass() throws Exception {
 	validSearchParams1.add("query", "testQueryString");
+	validEnrichJobsParams1.add("id", "[1234]");
+	discoverBody = Files.readString(Path.of("src/test/resources/discover-sample-body.json"));
+	enrichBody = Files.readString(Path.of("src/test/resources/enrich-sample-body.json"));
     }
 
     @AfterAll
@@ -91,7 +100,31 @@ class LiniusServiceImplTest {
     @Test
     @DisplayName("Send a single valid search, check status code")
     void searchValidBasicTest1() {
-	ResponseEntity<String> response = liniusService.search(testAccessToken, validSearchParams1);	
+	ResponseEntity<String> response = liniusService.search(testAccessToken, validSearchParams1);
+	assertNotNull(response);
+	assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Send a request for enrich jobs status, check status code")
+    void enrichJobsStatusCheck1() {
+	ResponseEntity<String> response = liniusService.enrichJobs(testAccessToken, validEnrichJobsParams1);
+	assertNotNull(response);
+	assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+    
+    @Test
+    @DisplayName("Send a request for discovery, check status code")
+    void discoveryStatusCheck1() {
+	ResponseEntity<String> response = liniusService.discover(testAccessToken, discoverBody);
+	assertNotNull(response);
+	assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+    
+    @Test
+    @DisplayName("Send a request for enriching, check status code")
+    void enrichStatusCheck1() {
+	ResponseEntity<String> response = liniusService.enrich(testAccessToken, enrichBody);
 	assertNotNull(response);
 	assertEquals(HttpStatus.OK, response.getStatusCode());
     }
